c9817d023a72c187a75d0ffc9678d5237626c02c
FDS1990 <f.disales@hotmail.com>
2017-10-23 12:53:04 +0200

    Add files via upload

---
+++ gameCamera.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameCamera : MonoBehaviour {
+
+    public GameObject gameobject;
+    private Vector3 offset;
+    public Camera camera;
+
+	// Use this for initialization
+	void Start ()
+    {
+        offset = transform.position - gameobject.transform.position;
+        camera = GetComponent<Camera>();
+    }
+	
+
+    public void ZoomCamera()
+    {
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+
+            camera.fieldOfView += 1f;
+
+
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+
+            camera.fieldOfView -= 1f;
+
+
+        }
+        
+    }
+
+    public void CameraTracking()
+    {
+
+        transform.position = gameobject.transform.position + offset;
+
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+        CameraTracking();
+        ZoomCamera();
+               
+
+    }
+
+    
+
+}
